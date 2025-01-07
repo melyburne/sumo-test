92d244a5bebfc342d625c66bdb86951f174774a5
Phuong Anh Duong <hduong@stud.hs-heilbronn.de>
2025-01-07 09:02:58 +0100

    uncomment stuff

--- src/simple_intersection_train_model.py
+++ src/simple_intersection_train_model.py
@@ -3,7 +3,7 @@ from simple_intersection_ppo import save_model as ppo_save_model
 from simple_intersection_random_agent import train_model as random_agent_train_model
 
 if __name__ == "__main__":
-    # dqn_save_model()
-    # ppo_save_model()
+    dqn_save_model()
+    ppo_save_model()
     # To compare rewards
     random_agent_train_model()
